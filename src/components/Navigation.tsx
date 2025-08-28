1e8353c4ebebce55df701a14f9ce3d1f4c77e1c3
isoqovjorabek2 <116886093+isoqovjorabek2@users.noreply.github.com>
2025-08-28 11:53:05 +0500

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -315,7 +315,7 @@ export const Navigation: React.FC<NavigationProps> = ({
                 >
                   {t.register}
                 </button>
-              className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+              </>
             )}
           </div>
         </div>
